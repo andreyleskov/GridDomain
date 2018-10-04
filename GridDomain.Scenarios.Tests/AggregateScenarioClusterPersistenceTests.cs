3223c0326585f1b64970a449652f7c259fa8d1d4
Andrey Leskov <andrey.leskov@gmail.com>
2018-10-04 23:50:45 +0800

    Scenario tests with persistence use connection string with ci server support

---
+++ GridDomain.Scenarios.Tests/AggregateScenarioClusterPersistenceTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using GridDomain.Configuration;
+using GridDomain.Scenarios.Runners;
+using GridDomain.Tests.Acceptance;
+using GridDomain.Tests.Unit;
+using GridDomain.Tests.Unit.BalloonDomain.Configuration;
+using GridDomain.Tests.Unit.ProcessManagers.SoftwareProgrammingDomain;
+using Xunit.Abstractions;
+
+namespace GridDomain.Scenarios.Tests {
+    public class AggregateScenarioClusterPersistenceTests : AggregateScenarioTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        protected override async Task<IAggregateScenarioRun<T>> Run<T>(IAggregateScenario<T> scenario)
+        {
+            
+            var cfg = new AutoTestNodeDbConfiguration();
+            await TestDbTools.Delete(cfg.JournalConnectionString, "Journal");
+
+            return await scenario.Run()
+                                 .Cluster(new DomainConfiguration(new BalloonDomainConfiguration(),
+                                                                  new ProgrammerAggregateDomainConfiguration()),
+                                          cfg.JournalConnectionString,
+                                          () =>new XUnitAutoTestLoggerConfiguration(_output));
+        }
+
+        public AggregateScenarioClusterPersistenceTests(ITestOutputHelper output) : base(output)
+        {
+            _output = output;
+        }
+    }
+}
---
+++ GridDomain.Scenarios.Tests/AggregateScenarioNodePersistenceTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using GridDomain.Configuration;
+using GridDomain.Scenarios.Runners;
+using GridDomain.Tests.Acceptance;
+using GridDomain.Tests.Unit.BalloonDomain.Configuration;
+using GridDomain.Tests.Unit.ProcessManagers.SoftwareProgrammingDomain;
+using Xunit.Abstractions;
+
+namespace GridDomain.Scenarios.Tests {
+    public class AggregateScenarioNodePersistenceTests : AggregateScenarioTests
+    {
+        protected override async Task<IAggregateScenarioRun<T>> Run<T>(IAggregateScenario<T> scenario)
+        {
+            var cfg = new AutoTestNodeDbConfiguration();
+            await TestDbTools.Delete(cfg.JournalConnectionString, "Journal");
+            
+            return await scenario.Run()
+                                 .Node(new DomainConfiguration(new BalloonDomainConfiguration(),
+                                                               new ProgrammerAggregateDomainConfiguration()),
+                                       Logger,
+                                       cfg.JournalConnectionString);
+        }
+
+        public AggregateScenarioNodePersistenceTests(ITestOutputHelper output) : base(output) { }
+    }
+}
