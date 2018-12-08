553dcb087fb9476aaae4da073349a6e86196f1e3
Andrey Leskov <andrey.leskov@gmail.com>
2018-12-09 01:02:24 +0800

    Additional output, restore tests execution

--- build/test.sh
+++ build/test.sh
@@ -6,9 +6,11 @@
       configuration=$3
 
       cd $testsFolder
+      echo Running tests in $testsFolder
       dotnet test --no-build --configuration $configuration --no-restore --logger trx > TestsOutput.txt
       TestResult=$?
       
+      echo Looking for a tests results
       cd ./bin/$configuration/netcoreapp2.0/Logs
       zip -r ./"$testsName"Logs.zip ./*
        
@@ -17,7 +19,7 @@
 
       if [ $TestResult -ne 0 ];then
          echo should exit...
-            exit #$TestResult
+            #exit #$TestResult
       fi
       echo continue 
 
